15a1bf861cfd1a508fcdd985af4880c72b8863d7
ecathomas <abp21001@byui.edu>
2024-05-03 10:33:54 -0500

    start temple album and edit get dates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,5 +4,5 @@ document.getElementsByTagName('p')[0].innerHTML = date;
 
 var lastModified = document.lastModified;
 let oLastModified = `Last Modified: ${lastModified} `;
-document.getElementsByTagName('p2')[1].innerHTML = oLastModified;
+document.getElementsByTagName('p2')[0].innerHTML = oLastModified;
 
