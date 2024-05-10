3b8f20a197b9d23ed244f24cd2250aee5b765cf0
ecathomas <abp21001@byui.edu>
2024-05-10 15:56:47 -0500

    kansas city small version

---
+++ scripts/place.js
@@ -0,0 +1,5 @@
+let year = new Date().getFullYear();
+var lastModified = document.lastModified;
+let footer = `&copy; ${year} 🌸 Evelyn Abplanalp Thomas 🌸 MO, USA \n Last Modified: ${lastModified}`;
+document.getElementsByTagName('p')[0].innerHTML = footer;
+
