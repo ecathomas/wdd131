dd9ea067e54f8472b0d2479acbf254d703575ad6
ecathomas <abp21001@byui.edu>
2024-05-11 15:54:07 -0500

    js for windchill in kansas city

--- scripts/place.js
+++ scripts/place.js
@@ -3,3 +3,12 @@ var lastModified = document.lastModified;
 let footer = `&copy; ${year} 🌸 Evelyn Abplanalp Thomas 🌸 MO, USA \n Last Modified: ${lastModified}`;
 document.getElementsByTagName('p')[0].innerHTML = footer;
 
+var temp= 79;
+var wSpeed= 7;
+var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+var windChill= Math.round(windChill);
+if (temp <= 50 && wSpeed>3) {
+    document.getElementById("windChill").innerHTML= `${windChill}&deg;`
+} else {
+    document.getElementById("windChill").innerHTML= `N/A`
+}
